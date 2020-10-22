065eb3fe3273fbc3085345912e46f3384e147282
khaldoon khaldi <khaldoon9kh9@gmail.com>
2020-10-22 23:49:22 +0300

    fixed files depending

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import HomePage from '../src/containers/HomePage'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HomePage />
     </div>
   );
 }
--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import NavBar from '.../components/NavBar'
-import HowDoesItWork from '.../components/HowDoesItWork'
-import Header from '.../components/Header'
-import GetStarted from '.../components/GetStarted'
-import Footer from '.../components/Footer'
-import Features from '.../components/Features'
-import ExploreBlogs from '.../components/ExploreBlogs'
-import CategoriesBrowes from '.../components/CategoriesBrowes'
-import Quotes from '.../components/Quotes'
+import NavBar from '../../components/NavBar'
+import HowDoesItWork from '../../components/HowDoesItWork'
+import Header from '../../components/Header'
+import GetStarted from '../../components/GetStarted'
+import Footer from '../../components/Footer'
+import Features from '../../components/Features'
+import ExploreBlogs from '../../components/ExploreBlogs'
+import CategoriesBrowes from '../../components/CategoriesBrowes'
+import Quotes from '../../components/Quotes'
 
 
 const HomePage = () => {
@@ -25,3 +25,5 @@ const HomePage = () => {
         </div>
     )
 };
+
+export default HomePage;
