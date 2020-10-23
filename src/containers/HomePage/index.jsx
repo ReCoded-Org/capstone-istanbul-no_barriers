3e84dd5f739f33ffa28ccc9f341c105224f37452
khaldoon9kh <khaldoon9kh9@gmail.com>
2020-10-23 17:34:46 +0300

    Build Repo Setup & HomePage (#12)
    * HomePage Setup
    
    * Add Bootstrab,FireStore,i18n Packages
    
    * "Add package and yarn.lock"
    
    * run yarn run fix
    
    * fixed firebase issues

---
+++ src/containers/HomePage/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Quotes from "../../components/Quotes";
+import NavBar from "../../components/NavBar";
+import HowDoesWork from "../../components/HowDoesWork";
+import Header from "../../components/Header";
+import GetStarted from "../../components/GetStarted";
+import Footer from "../../components/Footer";
+import Features from "../../components/Features";
+import ExploreBlogs from "../../components/ExploreBlogs";
+import BrowseCategory from "../../components/BrowseCategory";
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+      <HowDoesWork />
+      <BrowseCategory />
+      <Features />
+      <ExploreBlogs />
+      <Quotes />
+      <GetStarted />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import * as firebaseConfig from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_W5FFmcpoLooDBWggCdOaWdD20aacVXk",
+  authDomain: "no-barriers.firebaseapp.com",
+  databaseURL: "https://no-barriers.firebaseio.com",
+  projectId: "no-barriers",
+  storageBucket: "no-barriers.appspot.com",
+  messagingSenderId: "175981001977",
+  appId: "1:175981001977:web:c6062fce63c410c8d87628",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
