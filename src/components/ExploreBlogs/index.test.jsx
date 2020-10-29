8e99f8d34ccbbb21e4b09036bb88adad577a4dd9
fadime94 <fadime.ozdemir11@gmail.com>
2020-10-29 21:12:09 +0300

    change test description

--- src/components/ExploreBlogs/index.test.jsx
+++ src/components/ExploreBlogs/index.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import ExploreBlogs from "./index";
 
-it("renders correctly", () => {
+it("Explore Blogs renders correctly", () => {
   const tree = renderer.create(<ExploreBlogs />).toJSON();
   expect(tree).toMatchSnapshot();
 });
