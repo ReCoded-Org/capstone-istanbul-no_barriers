51d623f9b0e13ff065f65a8774d526c92179ee1c
Fadime Özdemir <fadime.ozdemir11@gmail.com>
2020-10-31 22:21:56 +0300

    Feat: add Navbar (#24)
    * add import Navbar, Nav,Button from react bootstrap
    
    * create index.css and add import in index.jxs
    
    * add comment for navbar view
    
    * add log status state
    
    * add Navabar, Navs and button with conditional rendering
    
    * run yarn fix
    
    * add color variables, blue and white, into index.css
    
    * add style navbar background color and color
    
    * add bootrstap link in the index.html's  head
    
    * run yarn fix
    
    * import bootstrap.min.css
    
    * add extra space before bootstrap css comment
    
    * run yarn fix
    
    * fix div container position
    
    * run yarn fix
    
    * add style for navbar links
    
    * fix style navbar links
    
    * add comments in navbar index.css
    
    * delete bootstrap link from index.html and add style for buttons in nav bar
    
    * fix shadow buttons
    
    * run yarn fix
    
    * delete isLogged functionality
    
    * add margin left for last button
    
    * run yarn fix
    
    * add navDropdown for languages
    
    * fix dropdown active  style
    
    * run yarn fix
    
    * fix padding in the dropdown menu
    
    * fix border of dropdown menu
    
    * create test file for navbar
    
    * write test for navbar
    
    * run yarn fix
    
    * delete extra comment
    
    * build: fix merge conflicts
    
    * Refactor: change dropdown title and add en new dropdown item
    
    * style: add extra space
    
    * style: delete comment
    
    * refractor: change position of I18 provider
    
    * style: delete important keyword
    
    * Style: fix style bug related to the dropdown menu
    
    * style : fix dropdown color bug
    
    * style: fix button press and dropdown menu colors
    
    * style: fix dropdown menu color
    
    * run yarn test
    
    * Feat: add translation in en file
    
    * refractor: change word in capital letters
    
    * Test: change test file name
    
    * test: pass the test
    
    * fix translation and run yarn fix
    
    * test: update snapshot
    
    * test: update snapshot test
    
    * run yarn fix
    
    * run yarn fix
    
    * add gitattributes for line ending
    
    * Normalize all the line endings
    
    * yarn run fix
    
    * yarn run fix
    
    * change position lang link, delete sign out button and update test
    
    * change order i18next
    
    * change classnames
    
    * update test
    
    * fix merge conflict in git ignore
    
    * delete extra space
    
    * delete margin class and add bootstrap class
    
    * update test
    
    * run yarn fix
    
    * change test description
    
    * delete obsolete test
    
    * delete extra space
    
    * add extra space
    
    * add git attribute
    
    * delete extra space
    
    * delete exploreBlogs related stuff
    
    * Revert "delete exploreBlogs related stuff"
    
    This reverts commit 2874d6f48c7a7e6d3e5880ede572303664694d8d.
    
    * add container component rather than className
    
    * add translation for explore blogs

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Navbar, Nav, NavDropdown, Button, Container } from "react-bootstrap";
+import "./index.css";
+import { useTranslation } from "react-i18next";
 
+// Navbar will display different links according to the log status of the user:
+//  if user is logged in => Find ngo, stories, about us, profile + only log out button
+//  otherwise => Find ngo, stories, about us + buttons for sign in and sign up
 const NavBar = () => {
-  return <div></div>;
+  const { t } = useTranslation();
+
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">{t("homePage.navbar.home")}</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#findNgo">{t("homePage.navbar.ngo")}</Nav.Link>
+          <Nav.Link href="#stories">{t("homePage.navbar.stories")}</Nav.Link>
+          <Nav.Link href="#aboutUs">{t("homePage.navbar.aboutUs")}</Nav.Link>
+          <Nav.Link href="#profile">{t("homePage.navbar.profile")}</Nav.Link>
+          <Button variant="outline-info" className="shadow-none">
+            {t("homePage.navbar.signIn")}
+          </Button>
+          <Button variant="outline-info" className="shadow-none mr-1">
+            {t("homePage.navbar.signUp")}
+          </Button>
+          <NavDropdown
+            title={t("homePage.navbar.dropdownTitle")}
+            id="basic-nav-dropdown"
+          >
+            <NavDropdown.Item href="#action/3.1">
+              {t("homePage.navbar.dropdownEnglish")}
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">
+              {t("homePage.navbar.dropdownArabic")}
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">
+              {t("homePage.navbar.dropdownTurkish")}
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
