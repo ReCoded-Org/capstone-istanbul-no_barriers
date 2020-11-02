6f47321e7473cf8520820a526eb22be02ab20a3e
fadime94 <fadime.ozdemir11@gmail.com>
2020-11-02 14:39:45 +0300

    feat: add search bar html structure

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Container, Button } from "react-bootstrap";
 
 const SearchBar = () => {
     return (
-        <div>
-            
+        <div className="globalSearch">
+            <Container>
+                <h1>Find the Service you Need</h1>
+                <div className="searchBar">
+                    <input className="searchBarInput" type="text" placeholder="Search for a service..." maxLength="15"/>
+                    <Button className="searchBarButton">Search</Button>
+                </div>
+            </Container>
         </div>
     );
 }
