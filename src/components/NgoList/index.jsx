80b36e02c29cd62c4b9e19f005b77547455c6b34
fadime94 <fadime.ozdemir11@gmail.com>
2020-11-02 14:40:15 +0300

    run yarn fix

--- src/components/NgoList/index.jsx
+++ src/components/NgoList/index.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const NgoList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return <div></div>;
+};
 
 export default NgoList;
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import {Container, Button } from "react-bootstrap";
+import React from "react";
+import { Container, Button } from "react-bootstrap";
 
 const SearchBar = () => {
-    return (
-        <div className="globalSearch">
-            <Container>
-                <h1>Find the Service you Need</h1>
-                <div className="searchBar">
-                    <input className="searchBarInput" type="text" placeholder="Search for a service..." maxLength="15"/>
-                    <Button className="searchBarButton">Search</Button>
-                </div>
-            </Container>
+  return (
+    <div className="globalSearch">
+      <Container>
+        <h1>Find the Service you Need</h1>
+        <div className="searchBar">
+          <input
+            className="searchBarInput"
+            type="text"
+            placeholder="Search for a service..."
+            maxLength="15"
+          />
+          <Button className="searchBarButton">Search</Button>
         </div>
-    );
-}
+      </Container>
+    </div>
+  );
+};
 
 export default SearchBar;
