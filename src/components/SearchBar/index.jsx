df7814ce56a2953cc70e13c29ab32b2a267e7c25
fadime94 <fadime.ozdemir11@gmail.com>
2020-11-02 17:05:50 +0300

    feat: add English translation

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
 import "./index.css";
+import { useTranslation } from "react-i18next";
 
 const SearchBar = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="globalSearch">
       <Container>
-        <h1>Find the Service you Need</h1>
+        <h1>{t("resources.searchBar.searchTitle")}</h1>
         <div className="searchBar d-flex">
           <input
             className="searchBarInput flex-grow-1"
             type="text"
-            placeholder="Please, enter a search term..."
+            placeholder={t("resources.searchBar.inputPlaceholder")}
             maxLength="15"
           />
-          <Button className="searchBarButton shadow-none">Search</Button>
+          <Button className="searchBarButton shadow-none">
+            {t("resources.searchBar.searchButton")}
+          </Button>
         </div>
       </Container>
     </div>
