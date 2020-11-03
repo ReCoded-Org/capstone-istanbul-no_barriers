a89d359abb4759611b98c1690da76c1a61de232c
fadime94 <fadime.ozdemir11@gmail.com>
2020-11-03 16:22:21 +0300

    run yarn fix

--- src/components/FilterOptionsList/index.jsx
+++ src/components/FilterOptionsList/index.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 
 // This renders the filter options of a given filter as parameter.
-// It is called in AdvancedFilters component 
-const FilterOptionsList = ({filter}) => {
-    return (
-        <div className="filterOptionsList">
-            {filter.filterParameters.map(option=>
-                return (<div className="filterOption d-flex">
-             
-                  <span className="filterOptionPlusSign">+</span>
-               
-                <p className="filterOptionTitle">{option}</p>
-              </div>)
-            )}
-        </div>
-    );
-}
+// It is called in AdvancedFilters component
+const FilterOptionsList = ({ filter }) => {
+  return (
+    <div className="filterOptionsList">
+      {filter.filterParameters.map((option) => {
+        return (
+          <div className="filterOption d-flex">
+            <span className="filterOptionPlusSign">+</span>
+
+            <p className="filterOptionTitle">{option}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default FilterOptionsList;
