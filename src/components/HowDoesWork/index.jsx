01fc5bbfc098f0efee8e518d82b4f6563170d5b1
Zeynep Sen <63185300+senzeynep@users.noreply.github.com>
2020-11-03 22:15:03 +0300

    add HowDoesWork component (#17)
    * add HowDoesWork component
    
    * fix: add alt attribute to img
    
    * Fix: add title to img
    
    add title and alt attributes to the img for accessibility,
    import bootstrap before the images for faster loading
    
    * Fix: add header
    
    * Test: add snapshot test
    
    * Feat: Add TrustedBy component
    
    * Fix: Add numbers to HowDoesWork component
    
    * Style: Add seperate CSS file for components
    
    couldn't run CircleCI yet
    
    * Fix: fix yarn run
    
    * Fix: trustedby component test
    
    * Fix: remove duplicate style from root CSS
    
    * Fix: yarn run fix add new line to end of file
    
    * Fix: update snapshot testing
    
    * Fix: change em into rem
    
    * Style: change CSS color variables
    
    * Style: update px into rem, move height to CSS
    
    * Fix: change snapshot test name
    
    * Fix: update name of the test
    
    * Fix: update class names
    
    * Fix: fix yarn start error
    
    * Test: put the test file under the component
    
    * Fix: update component name
    
    * Fix: delete unused ids, delete double import
    
    * Test: fix snapshot testing
    
    * Style: update tags, run test
    
    * Style: add a new line
    
    * Test: fix yarn test
    
    Co-authored-by: Halit Batur <61846570+halitbatur@users.noreply.github.com>

--- src/components/HowDoesWork/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HowDoesWork = () => {
-  return <div></div>;
-};
-
-export default HowDoesWork;
---
+++ src/components/TrustedBy/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import * as logo1 from "./932761.png";
+import * as logo2 from "./932762.png";
+import * as logo3 from "./932763.png";
+import * as logo4 from "./932764.png";
+import * as logo5 from "./932765.png";
+import "./index.css";
+
+const TrustedBy = () => {
+  return (
+    <Container fluid className="trustedBy">
+      <h4>Trusted by the World's Best NGOs</h4>
+      <br />
+      <Row>
+        <Col>
+          <img src={logo1} alt="logo1" title="logo1" />
+        </Col>
+        <Col>
+          <img src={logo2} alt="logo2" title="logo2" />
+        </Col>
+        <Col>
+          <img src={logo3} alt="logo3" title="logo3" />
+        </Col>
+        <Col>
+          <img src={logo4} alt="logo4" title="logo4" />
+        </Col>
+        <Col>
+          <img src={logo5} alt="logo5" title="logo5" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default TrustedBy;
