899d0b19864edba2bb9b2ac6023a767821cb83bd
fadime94 <fadime.ozdemir11@gmail.com>
2020-11-09 11:31:24 +0300

    run yarn fix

--- src/components/PagesList/index.jsx
+++ src/components/PagesList/index.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import {Pagination} from "react-bootstrap";
+import { Pagination } from "react-bootstrap";
 import allNgos from "../NgoList/allNgos";
- 
-const PagesList = ({ ngos = allNgos }) => {
 
-  const ngosPerPage= 10;
+const PagesList = ({ ngos = allNgos }) => {
+  const ngosPerPage = 10;
   const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(ngos.length / ngosPerPage); i++) {
-      pageNumbers.push(i);
-    }
+  for (let i = 1; i <= Math.ceil(ngos.length / ngosPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
   return (
     <div className="pages">
-    <Pagination className="d-flex justify-content-end pb-5" style={{
-    backgroundColor: "#eff4f6", paddingRight: "6rem", paddingTop: "1rem", marginBottom: "0"}}>
-      <Pagination.First />
-      {pageNumbers.map((number, index) => {
-        const displayedNumber = index + 1;
-        return (
-          <Pagination.Item key={displayedNumber} active={number === "active"}>
-            {number}
-          </Pagination.Item>
-        );
-      })}
-      <Pagination.Last />
-    </Pagination>
+      <Pagination
+        className="d-flex justify-content-end pb-5"
+        style={{
+          backgroundColor: "#eff4f6",
+          paddingRight: "6rem",
+          paddingTop: "1rem",
+          marginBottom: "0",
+        }}
+      >
+        <Pagination.First />
+        {pageNumbers.map((number, index) => {
+          const displayedNumber = index + 1;
+          return (
+            <Pagination.Item key={displayedNumber} active={number === "active"}>
+              {number}
+            </Pagination.Item>
+          );
+        })}
+        <Pagination.Last />
+      </Pagination>
     </div>
   );
 };
