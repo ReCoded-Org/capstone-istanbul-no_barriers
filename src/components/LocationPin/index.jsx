5b5cca6e3d190b3f629e2862d30d2837ec30589a
fadime94 <fadime.ozdemir11@gmail.com>
2020-11-09 14:36:34 +0300

    test: add test for location pin component

--- src/components/LocationPin/index.jsx
+++ src/components/LocationPin/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./index.css";
 
-const LocationPin = ({ text }) => (
+const LocationPin = ({ address }) => (
   <div className="pin">
     <span className="iconify" data-icon="mdi:map-marker" data-inline="false" />
-    <p className="pin-text">{text}</p>
+    <p className="pin-address">{address}</p>
   </div>
 );
 
