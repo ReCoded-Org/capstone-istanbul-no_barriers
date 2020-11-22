a59447825e69204103f172d09409da3b7d13c6e8
Khaldoon Khaldi <khaldoon9kh9@gmail.com>
2020-11-22 21:52:51 +0300

    fix errors (#99)

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -15,49 +15,52 @@ const NavBar = () => {
   };
 
   return (
-    <Navbar>
+    <Navbar collapseOnSelect="false" expand="lg">
       <NavLink to="/" className="navbar-brand">
         <Navbar.Brand>{t("homePage.navbar.home")}</Navbar.Brand>
       </NavLink>
-      <Nav className="ml-auto">
-        <NavLink to="/findngos" className="nav-link">
-          {t("homePage.navbar.ngo")}
-        </NavLink>
-        <NavLink to="/stories" className="nav-link">
-          {t("homePage.navbar.stories")}
-        </NavLink>
-        <NavLink to="/aboutUs" className="nav-link">
-          {t("homePage.navbar.aboutUs")}
-        </NavLink>
-        <NavLink to="/profile" className="nav-link">
-          {t("homePage.navbar.profile")}
-        </NavLink>
-        <NavLink to="/signin" className="nav-link">
-          <Button variant="outline-info" className="shadow-none mr-1">
-            {t("homePage.navbar.signIn")}
-          </Button>
-        </NavLink>
-        <NavLink to="/signup" className="nav-link">
-          <Button variant="outline-info" className="shadow-none mr-1">
-            {t("homePage.navbar.signUp")}
-          </Button>
-        </NavLink>
-        <NavDropdown
-          title={t("homePage.navbar.dropdownTitle")}
-          id="basic-nav-dropdown"
-          drop="left"
-        >
-          <NavDropdown.Item onClick={() => changeLanguage("en")}>
-            {t("homePage.navbar.dropdownEnglish")}
-          </NavDropdown.Item>
-          <NavDropdown.Item onClick={() => changeLanguage("ar")}>
-            {t("homePage.navbar.dropdownArabic")}
-          </NavDropdown.Item>
-          <NavDropdown.Item onClick={() => changeLanguage("tr")}>
-            {t("homePage.navbar.dropdownTurkish")}
-          </NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">
+          <NavLink to="/findngos" className="nav-link">
+            {t("homePage.navbar.ngo")}
+          </NavLink>
+          <NavLink to="/stories" className="nav-link">
+            {t("homePage.navbar.stories")}
+          </NavLink>
+          <NavLink to="/aboutUs" className="nav-link">
+            {t("homePage.navbar.aboutUs")}
+          </NavLink>
+          <NavLink to="/profile" className="nav-link">
+            {t("homePage.navbar.profile")}
+          </NavLink>
+          <NavLink to="/signin" className="nav.buttn">
+            <Button variant="outline-info" className="shadow-none mr-2 mb-1">
+              {t("homePage.navbar.signIn")}
+            </Button>
+          </NavLink>
+          <NavLink to="/signup" className="nav.buttn">
+            <Button variant="outline-info" className="shadow-none mr-1">
+              {t("homePage.navbar.signUp")}
+            </Button>
+          </NavLink>
+          <NavDropdown
+            title={t("homePage.navbar.dropdownTitle")}
+            id="basic-nav-dropdown"
+            drop="left"
+          >
+            <NavDropdown.Item onClick={() => changeLanguage("en")}>
+              {t("homePage.navbar.dropdownEnglish")}
+            </NavDropdown.Item>
+            <NavDropdown.Item onClick={() => changeLanguage("ar")}>
+              {t("homePage.navbar.dropdownArabic")}
+            </NavDropdown.Item>
+            <NavDropdown.Item onClick={() => changeLanguage("tr")}>
+              {t("homePage.navbar.dropdownTurkish")}
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
