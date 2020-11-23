3ca2dc4991d2b73ec2b57ae74163a553a87dc6d4
Khaldoon Khaldi <khaldoon9kh9@gmail.com>
2020-11-23 12:51:04 +0300

    Fix Fetch stories based on translation (#102)
    * fix translation
    
    * rv magic num
    
    * fix fetch repeat
    
    * change const names
    
    Co-authored-by: Halit Batur <61846570+halitbatur@users.noreply.github.com>

--- src/containers/Stories/index.jsx
+++ src/containers/Stories/index.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import SingleStoryCard from "../../components/SingleStoryCard";
 import LoadingPage from "../../components/LoadingPage";
+import i18next from "i18next";
 
 const Stories = () => {
+  const ENGLISH_LANGUAGE = "10221";
+  const AR_LANGUAGE = "38299";
+  const TR_LANGUAGE = "64053";
+  const [langRef, setLangRef] = React.useState(ENGLISH_LANGUAGE);
   const [posts, setPosts] = React.useState([]);
   const _URL =
     "https://public-api.wordpress.com/wp/v2/sites/nobarriers650349093.wordpress.com/posts";
@@ -17,18 +22,31 @@ const Stories = () => {
     fetchPosts();
   }, []);
 
+  i18next.on("languageChanged", (lng) => {
+    let ref = i18next.translator.language;
+    if (ref === "en") {
+      setLangRef(ENGLISH_LANGUAGE);
+    } else if (ref === "ar") {
+      setLangRef(AR_LANGUAGE);
+    } else if (ref === "tr") {
+      setLangRef(TR_LANGUAGE);
+    }
+  });
+
   if (!posts) {
     return <LoadingPage />;
   } else {
     return (
       <Container>
-        {posts.map((post, index) => {
-          if (index % 2 === 0) {
-            return <SingleStoryCard post={post} order="first" />;
-          } else {
-            return <SingleStoryCard post={post} order="last" />;
-          }
-        })}
+        {posts
+          .filter((post) => post.tags.toString() === langRef)
+          .map((post, index) => {
+            if (index % 2 === 0) {
+              return <SingleStoryCard post={post} order="first" />;
+            } else {
+              return <SingleStoryCard post={post} order="last" />;
+            }
+          })}
       </Container>
     );
   }
