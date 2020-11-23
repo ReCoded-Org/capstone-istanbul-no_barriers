da0677808a2b21b3f66a4af154050df4216a1054
Khaldoon Khaldi <khaldoon9kh9@gmail.com>
2020-11-23 13:44:15 +0300

    Add Translation to Get started (#108)
    * add translation
    
    * change mgic tex

--- src/components/GetStarted/index.jsx
+++ src/components/GetStarted/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,6 +8,7 @@ import Button from "react-bootstrap/Button";
 import "./index.css";
 
 const GetStarted = () => {
+  const { t } = useTranslation();
   const history = useHistory();
 
   const handleSignUp = () => history.push("/signup");
@@ -15,8 +17,8 @@ const GetStarted = () => {
     <Container fluid className="getStarted">
       <Row>
         <Col md="6">
-          <h6>Ready to get started?</h6>
-          <p>Sign up or contact us</p>
+          <h6>{t("homePage.getStarted.ready")}</h6>
+          <p>{t("homePage.getStarted.parag")}</p>
         </Col>
         <Col xs md="1.5">
           <Button
@@ -25,12 +27,12 @@ const GetStarted = () => {
             size="sm"
             onClick={handleSignUp}
           >
-            Sign Up
+            {t("homePage.getStarted.signUp")}
           </Button>
         </Col>
         <Col xs md="1.5">
           <Button className="contactUs" variant="primary" size="sm">
-            Contact Us
+            {t("homePage.getStarted.contactUs")}
           </Button>
         </Col>
         <Col xs md="3" />
