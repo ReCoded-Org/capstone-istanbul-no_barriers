5cce7c57ab5595eba1373dddd600237fdc661568
Khaldoon Khaldi <khaldoon9kh9@gmail.com>
2020-11-23 22:27:11 +0300

    Fix sign out (#117)
    * add redux to Nav
    
    * toggle between sign out and sign in
    
    * add translation

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { userLoggedInAction } from "../../redux/action";
+import { auth } from "../../firebaseConfig";
 import "./index.css";
 import { useTranslation } from "react-i18next";
 import i18n from "../../i18n";
@@ -17,6 +20,22 @@ const NavBar = () => {
     dispatch(languageAction(lng));
     i18n.changeLanguage(lng);
   };
+
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.currentUserDataReducer);
+  const [isLoggedOut, setIsLoggedOut] = React.useState(false);
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedOut(false);
+        dispatch(userLoggedInAction(user));
+      } else {
+        setIsLoggedOut(true);
+        dispatch(userLoggedInAction(null));
+      }
+    });
+  });
 
   return (
     <Navbar collapseOnSelect="false" expand="lg">
@@ -35,19 +54,56 @@ const NavBar = () => {
           <NavLink to="/aboutUs" className="nav-link">
             {t("homePage.navbar.aboutUs")}
           </NavLink>
-          <NavLink to="/profile" className="nav-link">
-            {t("homePage.navbar.profile")}
-          </NavLink>
-          <NavLink to="/signin" className="nav.buttn">
-            <Button variant="outline-info" className="shadow-none mr-2 mb-1">
-              {t("homePage.navbar.signIn")}
-            </Button>
-          </NavLink>
-          <NavLink to="/signup" className="nav.buttn">
-            <Button variant="outline-info" className="shadow-none mr-1">
-              {t("homePage.navbar.signUp")}
-            </Button>
-          </NavLink>
+
+          <div className="authToggleBtns">
+            {userData && (
+              <>
+                <NavDropdown
+                  title={t("homePage.navbar.user")}
+                  id="basic-nav-dropdown"
+                  drop="left"
+                >
+                  <Route
+                    render={({ history }) => (
+                      <NavDropdown.Item
+                        onClick={() => history.push("/profile")}
+                        eventKey="1"
+                      >
+                        {t("homePage.navbar.profile")}
+                      </NavDropdown.Item>
+                    )}
+                  />
+                  <Route
+                    render={({ history }) => (
+                      <NavDropdown.Item
+                        onClick={() => {
+                          auth.signOut();
+                          history.push("/");
+                        }}
+                        eventKey="1"
+                      >
+                        {t("homePage.navbar.signOut")}
+                      </NavDropdown.Item>
+                    )}
+                  />
+                </NavDropdown>
+              </>
+            )}
+            {isLoggedOut && (
+              <>
+                <NavLink to="/signin" className="nav.buttn">
+                  <Button variant="outline-info" className="shadow-none mr-2">
+                    {t("homePage.navbar.signIn")}
+                  </Button>
+                </NavLink>
+                <NavLink to="/signup" className="nav.buttn">
+                  <Button variant="outline-info" className="shadow-none mr-1">
+                    {t("homePage.navbar.signUp")}
+                  </Button>
+                </NavLink>
+              </>
+            )}
+          </div>
           <NavDropdown
             title={t("homePage.navbar.dropdownTitle")}
             id="basic-nav-dropdown"
--- src/components/NavBar/index.test.jsx
+++ src/components/NavBar/index.test.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allReducers from "../../redux/reducers";
+
 import Navbar from "./index";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
+
+const store = createStore(
+  allReducers,
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+);
 
 it("navbar renders correctly", () => {
   const tree = renderer
