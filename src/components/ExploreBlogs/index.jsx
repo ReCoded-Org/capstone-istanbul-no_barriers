00abc50a56ebb06e57705bd1fb4497cca9699962
Khaldoon Khaldi <khaldoon9kh9@gmail.com>
2020-11-23 12:54:00 +0300

    Fix home blogs (#106)
    * fix changing blogs on home bage based on lang
    
    * change const names
    
    * remove comment
    
    * fixed title rener error
    
    Co-authored-by: Halit Batur <61846570+halitbatur@users.noreply.github.com>

--- src/components/ExploreBlogs/index.jsx
+++ src/components/ExploreBlogs/index.jsx
@@ -3,13 +3,17 @@ import "./index.css";
 import { Row, Container, Col, Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import i18next from "i18next";
 
 // Explore-Blogs section shows the last 3 blog posts in the database.
 // If you click on a single blog post, you go to that blog's page
 // If you click on show more button, you go to blogs list page
 const ExploreBlogs = () => {
   const { t } = useTranslation();
-
+  const ENGLISH_LANGUAGE = "10221";
+  const AR_LANGUAGE = "38299";
+  const TR_LANGUAGE = "64053";
+  const [langRef, setLangRef] = React.useState(ENGLISH_LANGUAGE);
   const [posts, setPosts] = React.useState([]);
   const _URL =
     "https://public-api.wordpress.com/wp/v2/sites/nobarriers650349093.wordpress.com/posts";
@@ -20,10 +24,21 @@ const ExploreBlogs = () => {
     const fetchPosts = async () => {
       const data = await fetch(_URL);
       const fetchedPosts = await data.json();
-      setPosts(fetchedPosts.slice(_startNum, _endNum));
+      setPosts(fetchedPosts);
     };
     fetchPosts();
   }, []);
+
+  i18next.on("languageChanged", (lng) => {
+    let ref = i18next.translator.language;
+    if (ref === "en") {
+      setLangRef(ENGLISH_LANGUAGE);
+    } else if (ref === "ar") {
+      setLangRef(AR_LANGUAGE);
+    } else if (ref === "tr") {
+      setLangRef(TR_LANGUAGE);
+    }
+  });
 
   return (
     <div className="exploreBlogs">
@@ -41,22 +56,29 @@ const ExploreBlogs = () => {
       </Container>
       <Container className="exploreBlogsBlogList">
         <Row className="justify-content-center">
-          {posts.map((post) => {
-            return (
-              <Col xs={12} md={6} lg={3} className="exploreBlogsBlog">
-                <Link to={`/story/${post.id}`}>
-                  <img
-                    src={post.jetpack_featured_media_url}
-                    alt="placeholder"
-                    className="exploreBlogsBlogImg"
-                  />
-                  <h5 className="exploreBlogsBlogHeading">
-                    {post.title.rendered}
-                  </h5>
-                </Link>
-              </Col>
-            );
-          })}
+          {posts
+            .filter((post) => post.tags.toString() === langRef)
+            .slice(_startNum, _endNum)
+            .map((post) => {
+              console.log(post.title);
+              return (
+                <Col xs={12} md={6} lg={3} className="exploreBlogsBlog">
+                  <Link to={`/story/${post.id}`}>
+                    <img
+                      src={post.jetpack_featured_media_url}
+                      alt="placeholder"
+                      className="exploreBlogsBlogImg"
+                    />
+                    <h5
+                      className="exploreBlogsBlogHeading"
+                      dangerouslySetInnerHTML={{
+                        __html: post.title.rendered,
+                      }}
+                    ></h5>
+                  </Link>
+                </Col>
+              );
+            })}
         </Row>
       </Container>
       <Row className="justify-content-center">
